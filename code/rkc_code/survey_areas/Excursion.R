fdcbbd1b2cbae99895df96e840658eb3a599ade6
kjpalof <katie.palof@alaska.gov>
2019-07-30 15:30:48 -0800

    updates to file saving and raw sample function

--- code/rkc_code/survey_areas/Excursion.R
+++ code/rkc_code/survey_areas/Excursion.R
@@ -244,7 +244,7 @@ total_health('Excursion', cur_yr)
 head(dat5)
 dat5 %>% group_by(Year, Location) %>%  select(Year, Location, Juvenile, Small.Females, 
                                               Large.Females, Pre_Recruit, Recruit,Post_Recruit) %>% 
-  summarise_all(funs(sum)) -> raw_samp
+  summarise_all(sum) -> raw_samp
 
 dat5 %>% 
   group_by(Year) %>% 
@@ -268,11 +268,14 @@ CPUE_wt_all
 CPUE_wt_all %>% filter(Year >= 1993) -> CPUE_wt_from93
 
 write.csv(CPUE_wt_from93, paste0('results/rkc/', survey.location, '/', 
-                                 cur_yr, '/cpue_wt_all_yrs.csv'), row.names = FALSE)
+                                 cur_yr, '/cpue_wt_since_93.csv'), row.names = FALSE)
 
-panel_figure('Excursion', 2019, 'Excursion', 1) # panel with all 3 figures
-panel_figure('Excursion', 2019, 'Excursion', 2) # male panel
-panel_figure('Excursion', 2019, 'Excursion', 3) # female panel
+write.csv(CPUE_wt_all, paste0('results/rkc/', survey.location, '/', 
+                              cur_yr, '/cpue_wt_all_yrs.csv'), row.names = FALSE)
+
+panel_figure('Excursion', 2019, 'Excursion', 1, 0) # panel with all 3 figures
+panel_figure('Excursion', 2019, 'Excursion', 2, 0) # male panel
+panel_figure('Excursion', 2019, 'Excursion', 3. 0) # female panel
 # panel_figure <- function(survey.location, cur_yr, base.location)
 # base.location is the location name in the baseline file, can be different
 
