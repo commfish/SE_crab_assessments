a55102b5783ae977aa1ce63f6ea77e45794eaa34
kjpalof <katie.palof@alaska.gov>
2019-09-04 12:21:36 -0800

    figure creation and edits

--- code/rkc_code/survey_areas/Gambier.R
+++ code/rkc_code/survey_areas/Gambier.R
@@ -259,7 +259,7 @@ total_health('Gambier', cur_yr)
 head(dat5)
 dat5 %>% group_by(Year, Location) %>%  select(Year, Location, Juvenile, Small.Females, 
                                               Large.Females, Pre_Recruit, Recruit,Post_Recruit) %>% 
-  summarise_all(funs(sum)) -> raw_samp
+  summarise_all(sum) -> raw_samp
 
 dat5 %>% 
   group_by(Year) %>% 
@@ -284,19 +284,20 @@ CPUE_wt_all
 CPUE_wt_all %>% filter(Year >= 1993) -> CPUE_wt_from93
 
 write.csv(CPUE_wt_from93, paste0('results/rkc/', survey.location, '/', 
-                                 cur_yr, '/cpue_wt_all_yrs.csv'), row.names = FALSE)
+                                 cur_yr, '/cpue_wt_since_93.csv'), row.names = FALSE)
+write.csv(CPUE_wt_all, paste0('results/rkc/', survey.location, '/', 
+                              cur_yr, '/cpue_wt_all_yrs.csv'), row.names = FALSE)
 
-
-panel_figure('Gambier', 2018, 'Gambier', 1) # panel with all 3 figures
-panel_figure('Gambier', 2018, 'Gambier', 2) # male panel
-panel_figure('Gambier', 2018, 'Gambier', 3) # female panel
+panel_figure('Gambier', 2019, 'Gambier', 1, 0) # panel with all 3 figures
+panel_figure('Gambier', 2019, 'Gambier', 2, 0) # male panel
+panel_figure('Gambier', 2019, 'Gambier', 3, 0) # female panel
 
 # panel_figure <- function(survey.location, cur_yr, base.location)
 # base.location is the location name in the baseline file, can be different
 
 ### presentation figure -----
-panel_figure_NC_PRES('Gambier', 2018, 'Gambier', 2, 0)
-panel_figure_NC_PRES('Gambier', 2018, 'Gambier', 3, 0)
+panel_figure_NC_PRES('Gambier', 2019, 'Gambier', 2, 0)
+panel_figure_NC_PRES('Gambier', 2019, 'Gambier', 3, 0)
 
 ### female file all years -----
 # create females file for all years
