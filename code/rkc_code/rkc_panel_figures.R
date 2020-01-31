35133aa8f70dc181836e4bc85e53b76cf13bb9b8
kjpalof <katie.palof@alaska.gov>
2020-01-31 12:29:42 -0900

    se rkc panel figures creation. Move this code from each survey area.R file

---
+++ code/rkc_code/rkc_panel_figures.R
@@ -0,0 +1,26 @@
+# K. palof 
+# katie.palof@alaska.gov
+
+# Southeast RKC stock panel figures for assessment memo and RIR 
+
+# Notes: historically these were in each survey_area.R file but that is problematic when making
+#       changes for docs, etc. Now these are all together here. 
+
+# load ----------
+source('./code/functions.R')
+cur_yr <- 2019
+
+# Survey areas and associated figure calls:-------
+# Juneau 
+
+# Excursion
+
+# Lynn Sisters
+
+# Peril 
+
+# Pybus
+
+# Gambier
+
+# Seymour
