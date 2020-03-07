b13e82d9cba5a20fbc91563a8ab4a926172b1d4f
kjpalof <katie.palof@alaska.gov>
2020-03-06 15:14:35 -0900

    calls to create figures for non-confidential document versions for RIR

--- code/rkc_code/rkc_panel_figures.R
+++ code/rkc_code/rkc_panel_figures.R
@@ -11,16 +11,41 @@ source('./code/functions.R')
 cur_yr <- 2019
 
 # Survey areas and associated figure calls:-------
-# Juneau 
+# Juneau =============================
+### presentation figure -----
+panel_figure_NC_PRES('Juneau', cur_yr, 'Juneau', 2, 0, 'Juneau Area')
+panel_figure_NC_PRES('Juneau', cur_yr, 'Juneau', 3, 0, 'Juneau Area')
 
-# Excursion
+# Excursion ==========================
+### NON CONF panel --------------
+panel_figure_NC('Excursion', cur_yr, 'Excursion', 1, 0) # panel with all 3 figures
+panel_figure_NC('Excursion',  cur_yr, 'Excursion', 2, 0)
 
-# Lynn Sisters
+### presentation figure -----
+panel_figure_NC_PRES('Excursion', cur_yr, 'Excursion', 2, 0, 'Excursion Inlet')
+panel_figure_NC_PRES('Excursion', cur_yr, 'Excursion', 3, 0, 'Excursion Inlet')
 
-# Peril 
+# Lynn Sisters =======================
+### presentation figure -----
+panel_figure_NC_PRES('LynnSisters', cur_yr, 'LynnSisters', 2, 0, 'Lynn Sisters')
+panel_figure_NC_PRES('LynnSisters', cur_yr, 'LynnSisters', 3, 0, 'Lynn Sisters')
 
-# Pybus
+# Peril ==============================
+### presentation figure -----
+panel_figure_NC_PRES('Peril', cur_yr, 'Deadman Reach', 2, 0, 'Peril Strait')
+panel_figure_NC_PRES('Peril', cur_yr, 'Deadman Reach', 3, 0, 'Peril Strait')
 
-# Gambier
+# Pybus ==============================
+### presentation figure -----
+panel_figure_NC_PRES('Pybus', cur_yr, 'Pybus', 2, 2, 'Pybus Bay')
+panel_figure_NC_PRES('Pybus', cur_yr, 'Pybus', 3, 0, 'Pybus Bay') #female panel
 
-# Seymour
+# Gambier  ===========================
+### presentation figure -----
+panel_figure_NC_PRES('Gambier', cur_yr, 'Gambier', 2, 0, 'Gambier Bay')
+panel_figure_NC_PRES('Gambier', cur_yr, 'Gambier', 3, 0, 'Gambier Bay')
+
+# Seymour ============================
+### presentation figure -----
+panel_figure_NC_PRES('Seymour', cur_yr, 'Seymour Canal', 2, 1, "Seymour Canal")
+panel_figure_NC_PRES('Seymour', cur_yr, 'Seymour Canal', 3, 1, "Seymour Canal")
