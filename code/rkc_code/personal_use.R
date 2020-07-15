60bec181abf809758e01120c570b6f98852c434b
kjpalof <katie.palof@alaska.gov>
2020-07-14 21:48:08 -0800

    updates to code, also needed to seperate out into season and double check 2019 data.  catch is good, need to check on permit status accuracy

--- code/rkc_code/personal_use.R
+++ code/rkc_code/personal_use.R
@@ -15,6 +15,7 @@ prv_yr = cur_yr-1 # fishery year NOT survey year
 #####Load Data ---------------------------------------------------
 personal_use <- read.csv(paste0(here::here(), "/data/harvest/11-A rkc pu_catch_updated2020.csv"))
 permit_type <- read.csv(paste0(here::here(), "/data/harvest/PU RKC Juneau 2019 2020 permit status summary.csv"))
+# **FIX** get biologist to help summarize this in future...way to time consuming for me and they have to do it already.
 
 ## reported number ----
 # ** in order to get permits not returned that do NOT have catch need to click on "xyz" and select "include rows with only null values"
@@ -39,12 +40,19 @@ by_status %>%
 
 number_crab_by_status %>% 
   mutate(status = ifelse(Permit.Returned.Status == "Permit entered online", 1, 
-                         ifelse(Permit.Returned.Status == "Permit phoned in", 1, 
+                         ifelse(Permit.Returned.Status == "Permit not returned - some harvest reported", 1, 
                                 ifelse(Permit.Returned.Status == "Permit returned", 1, 
                                        ifelse(Permit.Returned.Status == "Permit returned - did not fish", 2, 
-                                              0))))) %>% 
+                                              0)))), 
+         status_desc = ifelse(status == 1, "returned & harvest reported", 
+                              ifelse(status == 2, "returned - did not fish", "not returned"))) %>% 
+  mutate(crab_year = ifelse(Year == cur_yr, 2020, 
+                            ifelse(Year == prv_yr & Season == "S", 2020, 
+                                   ifelse(Year == prv_yr-1 & Season == "S", 2019, 2019)))) %>% # need to group by crab year not calendar year
+  group_by(crab_year, status, status_desc) %>% 
+  summarise(number = sum(number), pots = sum(pots), n = sum(n))%>% 
   mutate(cpue = number/pots, cpue_permits = number/n) -> by_status_current
-write.csv(by_status_current, paste0('./results/redcrab/Juneau/personal_use_raw_summary_', cur_yr,'.csv'), row.names = FALSE)
+write.csv(by_status_current, paste0('./results/rkc/Juneau/personal_use_raw_summary_', cur_yr,'.csv'), row.names = FALSE)
 
 by_status_current %>% 
   summarise(sum(number, na.rm = TRUE)) -> total_c
