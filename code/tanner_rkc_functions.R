da4b25ce339da6f5a8bf673ccee8553a7b342a0b
kjpalof <katie.palof@alaska.gov>
2020-11-11 21:40:34 -0900

    temp updates ...need to load fgnr

--- code/tanner_rkc_functions.R
+++ code/tanner_rkc_functions.R
@@ -26,6 +26,9 @@ loadfonts(device="win")
 windowsFonts(Times=windowsFont("TT Times New Roman"))
 
 #theme_set(theme_sleek())
+theme_set(theme_bw(base_size=12,base_family='Times New Roman')+ 
+            theme(panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank()))
 
 ### short term function ----------------
 #input is file with last four years of data summarized by pot
