ea9f02683b79d043b53d56ee7b3d8e451188577e
kjpalof <katie.palof@alaska.gov>
2021-07-14 08:29:09 -0800

    updates to personal use code for Juneau area 11-A

--- code/rkc_code/personal_use.R
+++ code/rkc_code/personal_use.R
@@ -8,13 +8,13 @@
 #####Load Packages ---------------------------------
 library(tidyverse)
 library(xlsx)
-cur_yr = 2020 # survey year
+cur_yr = 2021 # survey year
 
 prv_yr = cur_yr-1 # fishery year NOT survey year
 
 #####Load Data ---------------------------------------------------
-personal_use <- read.csv(paste0(here::here(), "/data/harvest/11-A rkc pu_catch_updated2020.csv"))
-permit_type <- read.csv(paste0(here::here(), "/data/harvest/PU RKC Juneau 2019 2020 permit status summary.csv"))
+personal_use <- read.csv(paste0(here::here(), "/data/harvest/11-A rkc pu_catch_updated2021.csv"))
+permit_type <- read.csv(paste0(here::here(), "/data/harvest/PU RKC Juneau 2019 2020 2021 permit status summary.csv"))
 # **FIX** get biologist to help summarize this in future...way to time consuming for me and they have to do it already.
 
 ## reported number ----
@@ -46,11 +46,11 @@ number_crab_by_status %>%
                                               0)))), 
          status_desc = ifelse(status == 1, "returned & harvest reported", 
                               ifelse(status == 2, "returned - did not fish", "not returned"))) %>% 
-  mutate(crab_year = ifelse(Year == cur_yr, 2020, 
-                            ifelse(Year == prv_yr & Season == "S", 2020, 
-                                   ifelse(Year == prv_yr-1 & Season == "S", 2019, 2019)))) %>% # need to group by crab year not calendar year
+  mutate(crab_year = ifelse(Year == cur_yr, cur_yr, 
+                            ifelse(Year == prv_yr & Season == "S", cur_yr, 
+                                   ifelse(Year == prv_yr-1 & Season == "S", prv_yr, prv_yr)))) %>% # need to group by crab year not calendar year
   group_by(crab_year, status, status_desc) %>% 
-  summarise(number = sum(number), pots = sum(pots), n = sum(n))%>% 
+  summarise(number = sum(number, na.rm = T), pots = sum(pots, na.rm = T), n = sum(n))%>% 
   mutate(cpue = number/pots, cpue_permits = number/n) -> by_status_current
 write.csv(by_status_current, paste0('./results/rkc/Juneau/personal_use_raw_summary_', cur_yr,'.csv'), row.names = FALSE)
 
