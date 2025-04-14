ded242544b7b4de3bd9ed88fe3e79e9fc2cc9b01
Alex Reich <alex.reich@alaska.gov>
2025-04-14 12:52:43 -0800

    reorg

--- 2024/code/rkc/RTMB/RTMB_CSA_Juneau.R
+++ 2024/code/rkc/RTMB/RTMB_CSA_Juneau.R
@@ -2,4 +2,24 @@
 ##Alex Reich
 ##start of development: 4/14/25
 ##recent work: 4/14/25
-##DRAFT###
+##IN DEVELOPMENT###
+
+
+
+#load libraries
+
+
+#load in datas
+
+
+####################################################################
+#The CSA part
+
+
+######################################################################
+#update the misc Juneau tables that I need (parallel the CSV) (only for the RKC juneau survey area)
+
+
+
+##################################################################
+#the update the biomass csv part
---
+++ 2024/code/rkc/readme.txt
@@ -0,0 +1,3 @@
+Step 1: The survey_area R code
+Step 2: The area_summary rmd code
+Step 3: The RTMB code
