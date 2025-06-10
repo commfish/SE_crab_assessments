bddcdfbe23de6a6e0db589a538fa0c3182e9cfd2
Alex Reich <alex.reich@alaska.gov>
2025-06-10 09:29:34 -0800

    added the outline

---
+++ code/RTMB_retros.R
@@ -0,0 +1,36 @@
+##retros##
+##quasi-retros, anyway
+##SE RKC RTMB
+##Alex Reich
+##6/10/25
+
+#libraries
+
+#loop it
+##get rid of one year every time
+##THAT GOES HERE
+
+
+####the model, paramters
+
+#model
+
+#params and data
+
+#run it
+
+#save it
+
+#####end the actual model run
+
+#end loop
+
+
+
+
+#####graph it
+#load in "retro" by-year CSA estimate for comparison
+
+#load in this year's RTMB estimate for comparison
+
+#graph!!
