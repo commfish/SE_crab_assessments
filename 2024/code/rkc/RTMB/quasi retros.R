3c8ae19aa4e371802a434f6f8e83017a0cfef166
Alex Reich <alex.reich@alaska.gov>
2025-06-16 11:08:22 -0800

    drafted the quasi retros R file

---
+++ 2024/code/rkc/RTMB/quasi retros.R
@@ -0,0 +1,23 @@
+######################3
+# RETROS RETROS RETROS (quasi)
+## CS asked what this analysis would look like if we did it past years
+#started: 6/16/25
+## author: Alex Reich
+###########################
+#set up some bins
+
+#for loop 
+#for i in 1:20{ #I cant imagine she wants to know the ENTIRE timeseries.
+##why not tho
+
+#remove one row from the data
+
+#run the analysis
+
+#store the result
+
+#}#end for loop
+
+###Graph
+#load in the "each year we predicted this" data
+#graph that vs the df I just created
