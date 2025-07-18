4a54678728ee829ab2b91eabb36e4132b3d99fc4
Alex Reich <alex.reich@alaska.gov>
2025-07-18 12:20:32 -0800

    NOW I fixed the x axis

--- code/functions.R
+++ code/functions.R
@@ -473,7 +473,7 @@ panel_figure <- function(survey.location, cur_yr, base.location, option, scale){
              )+
     ylab("CPUE (number/pot)")+ xlab(NULL)+
     theme(axis.text.x = element_blank(), plot.title = element_text(hjust =0.5)) + 
-    scale_x_continuous(breaks = seq(min(1996),max(cur_yr), by =2)) + #changed from min(1995) so my graphs will end at 2024 - ar
+    scale_x_continuous(breaks = seq(min(1995),max(cur_yr), by =2)) + #changed from min(1995) so my graphs will end at 2024 - ar
     geom_ribbon(aes(ymin = mean - se, ymax = mean + se), 
                 alpha = 0.2) +
     #geom_errorbar(aes(ymin = mean - se, ymax = mean + se, color = recruit.class), 
@@ -515,7 +515,7 @@ panel_figure <- function(survey.location, cur_yr, base.location, option, scale){
     scale_y_continuous(limits = c(0,(max(round(femjuv_graph$mean, 0) +1))), oob = rescale_none) +
     ylab("CPUE (number/pot)")+ xlab(NULL)+
     theme(axis.text.x = element_blank(), plot.title = element_text(hjust =0.5)) + 
-    scale_x_continuous(breaks = seq(min(1996),max(cur_yr), by =2)) + #changed from min(1995) so my graphs will end at 2024 - ar
+    scale_x_continuous(breaks = seq(min(1995),max(cur_yr), by =2)) + #changed from min(1995) so my graphs will end at 2024 - ar
     geom_ribbon(aes(ymin = mean - se, ymax = mean + se), 
                 alpha = 0.2) +
     #geom_errorbar(aes(ymin = mean - se, ymax = mean + se, color = recruit.class), 
@@ -564,7 +564,7 @@ panel_figure <- function(survey.location, cur_yr, base.location, option, scale){
     ylab("Percentage") + 
     xlab(NULL) +
     theme(plot.title = element_text(hjust =0.5)) + 
-    scale_x_continuous(breaks = seq(min(1996),max(cur_yr), by =2)) + #changed from min(1995) so my graphs will end at 2024 - ar
+    scale_x_continuous(breaks = seq(min(1995),max(cur_yr), by =2)) + #changed from min(1995) so my graphs will end at 2024 - ar
     theme(legend.position = c(0.2,0.5), 
           axis.text = element_text(size = 12), 
           axis.title=element_text(size=14,face="bold")) 
@@ -607,7 +607,7 @@ panel_figure <- function(survey.location, cur_yr, base.location, option, scale){
     ylab("Biomass (lb)") + 
     xlab("Year") +
     theme(plot.title = element_text(hjust =0.5)) + 
-    scale_x_continuous(breaks = seq(min(1996),max(cur_yr), by =2)) + #changed from min(1995) so my graphs will end at 2024 - ar
+    scale_x_continuous(breaks = seq(min(1995),max(cur_yr), by =2)) + #changed from min(1995) so my graphs will end at 2024 - ar
     #scale_y_continuous(labels = comma, limits = c(0,max(biomass_graph$pounds, 
     #                                                    na.rm = TRUE) + 25000),
      #                  breaks= seq(min(0), max(max(biomass_graph$pounds, 
@@ -648,7 +648,7 @@ panel_figure <- function(survey.location, cur_yr, base.location, option, scale){
       ylab("Biomass (lb)") + 
       xlab("Year") +
       theme(plot.title = element_text(hjust =0.5)) + 
-      scale_x_continuous(breaks = seq(min(1996),max(cur_yr), by =2)) + #changed from min(1995) so my graphs will end at 2024 - ar
+      scale_x_continuous(breaks = seq(min(1995),max(cur_yr), by =2)) + #changed from min(1995) so my graphs will end at 2024 - ar
       scale_y_continuous(labels = comma, limits = c(0,max(biomass_graph$pounds, 
                                                           na.rm = TRUE) + 25000),
                          breaks= seq(min(0), max(max(biomass_graph$pounds, 
@@ -678,7 +678,7 @@ panel_figure <- function(survey.location, cur_yr, base.location, option, scale){
       ylab("Biomass (lb)") + 
       xlab("Year") +
       theme(plot.title = element_text(hjust =0.5)) + 
-      scale_x_continuous(breaks = seq(min(1996),max(cur_yr), by =2)) + #changed from min(1995) so my graphs will end at 2024 - ar
+      scale_x_continuous(breaks = seq(min(1995),max(cur_yr), by =2)) + #changed from min(1995) so my graphs will end at 2024 - ar
       scale_y_continuous(labels = comma, limits = c(0,max(biomass_graph$pounds, 
                                                           na.rm = TRUE) + 25000),
                          breaks= seq(min(0), max(max(biomass_graph$pounds, 
