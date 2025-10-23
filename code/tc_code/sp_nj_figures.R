f7eb32ebc4a3bfa679afe47121c23daa3e7d7d4f
Alex Reich <alex.reich@alaska.gov>
2025-10-23 13:07:29 -0800

    fixed the scale

--- code/tc_code/sp_nj_figures.R
+++ code/tc_code/sp_nj_figures.R
@@ -259,7 +259,7 @@ panel_figure_J <- function(survey.location, cur_yr, area, abrv, option, conf){
   }
   
   ### biomass harvest graph --------------
-  p4 <- ggplot(biomass_graph, aes(Year, y = pounds/100000, group = type))+ 
+  p4 <- ggplot(biomass_graph, aes(Year, y = pounds, group = type))+ 
     geom_point(aes(color = type, shape = type), size =3) +
     geom_line(aes(color = type, group = type, linetype = type))+
     scale_colour_manual(name = "", values = c("grey1", "grey1", "grey48"),
@@ -272,14 +272,14 @@ panel_figure_J <- function(survey.location, cur_yr, area, abrv, option, conf){
     xlab("Year") +
     theme(plot.title = element_text(hjust =0.5)) + 
     scale_x_continuous(limits = c(1997, cur_yr), breaks = seq(min(1993),max(cur_yr), by =2)) + #changed 1993 to 1994 for even-year numbering
-    scale_y_continuous(labels = comma, limits = c(0,max(biomass_graph$pounds/100000, 
-                                                        na.rm = TRUE) + 0.25000),
-                       breaks= seq(min(0), max(max(biomass_graph$pounds/100000, 
-                                                   na.rm = TRUE)+0.25000), by = 1.0)) +
+    scale_y_continuous(labels = comma, limits = c(0,max(biomass_graph$pounds, 
+                                                        na.rm = TRUE) + 25000),
+                       breaks= seq(min(0), max(max(biomass_graph$pounds, 
+                                                   na.rm = TRUE)+25000), by = 100000)) +
     theme(legend.position = c(0.55,0.8), 
           axis.text = element_text(size = 12), 
           axis.title=element_text(size=14,face="bold")) + 
-    geom_hline(data = baseline_means, aes(yintercept = legal_mean/100000), color = "grey1", 
+    geom_hline(data = baseline_means, aes(yintercept = legal_mean), color = "grey1", 
                linetype = "dashed")
   #geom_hline(data = baseline_means, aes(yintercept = legal_adj_mean), color = "grey62", linetype = "dashed")
   #if(scale == 1){
