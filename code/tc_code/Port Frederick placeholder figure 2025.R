a3d19667b6a06fb0014649c75c7521886d68b4c2
Alex Reich <alex.reich@alaska.gov>
2025-11-04 15:44:42 -0900

    made a scratch PF fig

--- code/tc_code/Port Frederick placeholder figure 2025.R
+++ code/tc_code/Port Frederick placeholder figure 2025.R
@@ -89,6 +89,50 @@ dat3 %>%
 CPUE_fred_25_male <- CPUE_all %>% select(Year, Pre_Recruit_u, Recruit_u, Post_Recruit_u) %>%
   dplyr::rename(Survey.Year = Year, Pre.recruit = Pre_Recruit_u, Recruit = Recruit_u, Post.recruit = Post_Recruit_u)
 
-df_cpue_fred <- rbind(PF_old_cpue, CPUE_fred_25_male)
+df_cpue_fred <- rbind(PF_old_cpue, CPUE_fred_25_male) %>%
+  dplyr::rename(Year = Survey.Year, Prerecruit = Pre.recruit, Postrecruit = Post.recruit)
 #nice, that's my dataframe for graphing
+
+area = "Port Frederick"
+
+#graph
+PF_gg_scratch <- ggplot(df_cpue_fred)+ 
+  geom_point(aes(y= Prerecruit, x= Year), shape=16, size =3, color = "#E69F00",
+             fill = "#E69F00") +
+  geom_line(aes(y= Prerecruit, x= Year), color = "#E69F00")+
+  geom_point(aes(y= Recruit, x= Year), shape=17, size =3, color = "#56B4E9",
+             fill = "#56B4E9") +
+  geom_line(aes(y= Recruit, x= Year), color = "#56B4E9")+
+  geom_point(aes(y= Postrecruit, x= Year), shape=15, size =3, color = "#999999",
+             fill = "#999999") +
+  geom_line(aes(y= Postrecruit, x= Year), color = "#999999")+
+  
+  annotate("text", label = area, 
+           x = 2013.5, y = Inf, hjust = -0.05, vjust = 1.1,  # fine-tune the positioning
+           size = 6, fontface = "bold"    
+  )+ 
+  
+  ylab("CPUE (number/pot)")+ xlab(NULL)+
+  #theme(axis.text.x = element_blank(), plot.title = element_text(hjust =0.5)) + 
+  scale_x_continuous(limits = c(1997, cur_yr), breaks = seq(min(1993),max(cur_yr), by =2)) + #seq(min(1994...)) for even years and 1993 for odd years
+  #scale_y_continuous(limits = c(0,20), breaks =c(0, 5, 10, 15, 20))+
+  geom_hline(yintercept = scratch_baseline$Prerec_base, color = "#E69F00", 
+             linetype = "dotdash", lwd = 0.75)+
+  geom_hline(yintercept = scratch_baseline$Rec_base, color = "#56B4E9", 
+             linetype = "longdash", lwd = 0.75)+
+  geom_hline(yintercept = scratch_baseline$Postrec_base, color = "#999999",
+             lwd = 0.75)+
+  scale_y_continuous(labels = comma) +
+  theme(legend.position = c(0.44,0.85), 
+        axis.text = element_text(size = 12), 
+        axis.title=element_text(size=14,face="bold"), 
+        plot.title = element_text(size = 24))
+
+
+ggsave("figures/tanner/tanner_rkc/2025/PF_scratch.png", PF_gg_scratch)
+
+
+
+
+
 
